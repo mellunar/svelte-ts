759f61114c84f9a0034883c8f034c43365cd9b29
mellunar <melfer100@gmail.com>
2022-01-06 14:59:46 -0300

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { t } from "svelte-i18n";
+	import { Router } from "@roxi/routify";
+  import { routes } from "../.routify/routes";
+</script>
+
+<style global lang="scss">
+	@import './app.scss';
+</style>
+
+<nav class="nav">
+	<a href='/'>Home</a>
+	<a href='/about'>{$t('about.title')}</a>
+</nav>
+<Router {routes} />
