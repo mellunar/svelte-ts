dda16b88f4bf227f59e50d185077a094230446f5
Melissa Fernandes <melfer100@gmail.com>
2022-08-09 14:32:36 -0300

    fix: routify integration with vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import autoPreprocess from 'svelte-preprocess';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import autoPreprocess from "svelte-preprocess";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte({
-    preprocess: autoPreprocess()
-  })]
-})
+  server: {
+    port: 5000,
+  },
+  plugins: [
+    svelte({
+      preprocess: autoPreprocess(),
+    }),
+  ],
+});
