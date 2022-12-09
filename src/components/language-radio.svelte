5a8c5634dc2d8144d64889eadc8382bb233386ab
mellunar <melfer100@gmail.com>
2022-12-08 22:36:00 -0300

    chore: prettier format

--- src/components/language-radio.svelte
+++ src/components/language-radio.svelte
@@ -1,25 +1,25 @@
 <script>
-  import { onMount } from "svelte";
-  import { locale } from "svelte-i18n";
+  import { onMount } from 'svelte';
+  import { locale } from 'svelte-i18n';
   export let iso;
   let userLocale, lang;
   onMount(() => {
-    locale.subscribe((x) => userLocale = x);
+    locale.subscribe((x) => (userLocale = x));
     lang = userLocale.includes(iso);
-  })
+  });
   function setLocale() {
     locale.set(iso);
     localStorage.setItem('language', iso);
   }
 </script>
 
+<label for={`language-${iso}`} class={$$props.class}>
+  <input type="radio" name="language" id={`language-${iso}`} checked={lang} on:change={() => setLocale()} />
+  <span>{iso}</span>
+</label>
+
 <style>
   span {
     text-transform: uppercase;
   }
 </style>
-
-<label for={`language-${iso}`} class={$$props.class}>
-	<input type="radio" name="language" id={`language-${iso}`} checked={lang} on:change={() => setLocale()}>
-	<span>{iso}</span>
-</label>
